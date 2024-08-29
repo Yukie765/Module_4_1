13d240b44f4160b51ca112dc602fbad593205f14
Yukie765 <yliya-kate@mail.ru>
2024-08-29 16:49:47 +0500

    Add files via upload

---
+++ Fake_math.py
@@ -0,0 +1,5 @@
+def divide(a,b):
+    if b == 0:
+        return 'Ошибка'
+    else:
+        return round(a/b, 2)
---
+++ Module_4_1.py
@@ -0,0 +1,7 @@
+import True_math as tm
+import Fake_math as fm
+
+print(fm.divide(69, 3))
+print(fm.divide(3, 0))
+print(tm.divide(49, 7))
+print(tm.divide(15, 0))
